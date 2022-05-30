25d91a0747d61514de427873ab12b70c047c416e
RedTTG <redtonehair@gmail.com>
2022-05-30 19:09:53 +0300

    bios

--- Installer/bios.lua
+++ Installer/bios.lua
@@ -1,29 +1,64 @@
-local result, reason = ""
-
+local init
 do
-	local handle, chunk = component.proxy(component.list("internet")() or error("Required internet component is missing")).request("https://raw.githubusercontent.com/JustRedTTG/CraftOS/main/Installer/install.lua")
+  local component_invoke = component.invoke
+  local function boot_invoke(address, method, ...)
+    local result = table.pack(pcall(component_invoke, address, method, ...))
+    if not result[1] then
+      return nil, result[2]
+    else
+      return table.unpack(result, 2, result.n)
+    end
+  end
 
-	while true do
-		chunk = handle.read(math.huge)
-		
-		if chunk then
-			result = result .. chunk
-		else
-			break
-		end
-	end
+  -- backwards compatibility, may remove later
+  local eeprom = component.list("eeprom")()
+  computer.getBootAddress = function()
+    return boot_invoke(eeprom, "getData")
+  end
+  computer.setBootAddress = function(address)
+    return boot_invoke(eeprom, "setData", address)
+  end
 
-	handle.close()
+  do
+    local screen = component.list("screen")()
+    local gpu = component.list("gpu")()
+    if gpu and screen then
+      boot_invoke(gpu, "bind", screen)
+    end
+  end
+  local function tryLoadFrom(address)
+    local handle, reason = boot_invoke(address, "open", "/init.lua")
+    if not handle then
+      return nil, reason
+    end
+    local buffer = ""
+    repeat
+      local data, reason = boot_invoke(address, "read", handle, math.huge)
+      if not data and reason then
+        return nil, reason
+      end
+      buffer = buffer .. (data or "")
+    until not data
+    boot_invoke(address, "close", handle)
+    return load(buffer, "=init")
+  end
+  local reason
+  if computer.getBootAddress() then
+    init, reason = tryLoadFrom(computer.getBootAddress())
+  end
+  if not init then
+    computer.setBootAddress()
+    for address in component.list("filesystem") do
+      init, reason = tryLoadFrom(address)
+      if init then
+        computer.setBootAddress(address)
+        break
+      end
+    end
+  end
+  if not init then
+    error("no bootable medium found" .. (reason and (": " .. tostring(reason)) or ""), 0)
+  end
+  computer.beep(1000, 0.2)
 end
-
-result, reason = load(result, "=installer")
-
-if result then
-	result, reason = xpcall(result, debug.traceback)
-
-	if not result then
-		error(reason)
-	end
-else
-	error(reason)	
-end
+init()
