63ffecd3a3c7e771231064a8477f31823c607c0c
RedTTG <redtonehair@gmail.com>
2022-05-31 20:50:31 +0300

    test for better center text

--- Installer/install.lua
+++ Installer/install.lua
@@ -105,7 +105,7 @@ local function centerOf(width)
 	return math.floor(screenWidth/2 - width/2)
 end
 local function centerText(y, color, text)
-	component.invoke(gpuAddress, "fill", 1, y, screenWidth, 1, " ")
+	component.invoke(gpuAddress, "fill", centerOf(#text) , y, centerOf(#text)+#text, 1, " ")
 	component.invoke(gpuAddress, "setForeground", color)
 	component.invoke(gpuAddress, "set", centerOf(#text), y, text)
 end
