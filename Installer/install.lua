58bf8de1c9ce95593e32500469e1ab8472f99a32
RedTTG <redtonehair@gmail.com>
2022-05-31 18:29:23 +0300

    configuration, install, download

--- Installer/install.lua
+++ Installer/install.lua
@@ -8,15 +8,93 @@ local EEPROMAddress, internetAddress, GPUAddress =
 	getComponentAddress("internet"),
 	getComponentAddress("gpu")
 
+-- Get Ready ~
+do
+local addr, invoke = computer.getBootAddress(), component.invoke
+  local function loadfile(file)
+    local handle = assert(invoke(addr, "open", file))
+    local buffer = ""
+    repeat
+      local data = invoke(addr, "read", handle, math.huge)
+      buffer = buffer .. (data or "")
+    until not data
+    invoke(addr, "close", handle)
+    return load(buffer, "=" .. file, "bt", _G)
+  end
+end
+
+-- Internet
+local function rawRequest(url, chunkHandler)
+	local internetHandle, reason = component.invoke(internetAddress, "request", repositoryURL .. url:gsub("([^%w%-%_%.%~])", function(char)
+		return string.format("%%%02X", string.byte(char))
+	end))
+
+	if internetHandle then
+		local chunk, reason
+		while true do
+			chunk, reason = internetHandle.read(math.huge)	
+			
+			if chunk then
+				chunkHandler(chunk)
+			else
+				if reason then
+					error("Internet request failed: " .. tostring(reason))
+				end
+
+				break
+			end
+		end
+
+		internetHandle.close()
+	else
+		error("Connection failed: " .. url)
+	end
+end
+local function request(url)
+	local data = ""
+	
+	rawRequest(url, function(chunk)
+		data = data .. chunk
+	end)
+
+	return data
+end
+local function download(url, path)
+	selectedFilesystemProxy.makeDirectory(filesystemPath(path))
+
+	local fileHandle, reason = selectedFilesystemProxy.open(path, "wb")
+	if fileHandle then	
+		rawRequest(url, function(chunk)
+			selectedFilesystemProxy.write(fileHandle, chunk)
+		end)
+
+		selectedFilesystemProxy.close(fileHandle)
+	else
+		error("File opening failed: " .. tostring(reason))
+	end
+end
+
 -- Binding GPU to screen
 component.invoke(GPUAddress, "bind", getComponentAddress("screen"))
 local screenWidth, screenHeight = component.invoke(GPUAddress, "getResolution")
 
 local repositoryURL = "https://raw.githubusercontent.com/JustRedTTG/CornerOS/main/"
+local installerURL = "Installer"
 
-component.invoke(GPUAddress, "setBackground", 0x06181C)
-component.invoke(GPUAddress, "fill", 1, 1, screenWidth, screenHeight, " ")
-
-while true do
-computer.pullSignal()
+local function background(color)
+	component.invoke(GPUAddress, "setBackground", color)
+	component.invoke(GPUAddress, "fill", 1, 1, screenWidth, screenHeight, " ")
 end
+local function getColor(pallet, color)
+	for i in #pallet do
+		if pallet[i].name == color then
+			return pallet[i].color
+		end
+	end
+	return nil
+end
+
+-- Begin download
+progress(0)
+local config = deserialize(request(installerURL .. "config.cfg"))
+background(getColor(config.mainColors, "background"))
