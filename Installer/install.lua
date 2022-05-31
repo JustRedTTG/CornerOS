39ebf345cf05f5fa3e95baa35d3f5c9b82354ffb
RedTTG <redtonehair@gmail.com>
2022-05-31 22:47:12 +0300

    final debug, remove range ewww

--- Installer/install.lua
+++ Installer/install.lua
@@ -30,26 +30,6 @@ local function deserialize(text)
 		error(reason)
 	end
 end
-function range(a, b, step)
-  if not b then
-    b = a
-    a = 1
-  end
-  step = step or 1
-  local f =
-    step > 0 and
-      function(_, lastvalue)
-        local nextvalue = lastvalue + step
-        if nextvalue <= b then return nextvalue end
-      end or
-    step < 0 and
-      function(_, lastvalue)
-        local nextvalue = lastvalue + step
-        if nextvalue >= b then return nextvalue end
-      end or
-      function(_, lastvalue) return lastvalue end
-  return f, nil, a - step
-end
 
 -- Internet
 local config
@@ -166,10 +146,10 @@ status("Please wait...", config.mainColors.text)
 
 
 background(config.mainColors.background, config.mainColors.backgroundUpper, config.mainColors.backgroundMidrange, config)
-while debug do
+if debug then
 	status("Debug Screen", config.mainColors.text)
-	for i in range(10,100) do
-		progress(i/100, config)
-		computer.pullSignal()
-	end
+	progress(0.25, config)
+	repeat
+		needWait = computer.pullSignal()
+	until needWait == "key_down" or needWait == "touch"
 end
