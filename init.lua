636f067497f70995d5ec0ce6679ae59bdbb4b0f4
RedTTG <redtonehair@gmail.com>
2022-05-31 18:03:09 +0300

    delete it

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
+os.execute("rm /corner.lua")
 os.execute("wget -f https://raw.githubusercontent.com/JustRedTTG/CornerOS/main/Installer/install.lua /corner.lua")
 os.execute("wget -f https://raw.githubusercontent.com/JustRedTTG/CornerOS/main/BIOS/bios.lua /tmp/bios.lua && flash -q /tmp/bios.lua CornerOS && reboot")
