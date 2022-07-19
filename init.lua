542cdf518453d3c002123a5c033a140daee70456
RedTTG <redtonehair@gmail.com>
2022-07-19 13:59:03 +0300

    little welcome message

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+os.execute("clear")
+os.execute("echo Flashing Corner OS")
 os.execute("rm /corner.lua")
 os.execute("wget -f https://raw.githubusercontent.com/JustRedTTG/CornerOS/main/Installer/install.lua /corner.lua")
 os.execute("wget -f https://raw.githubusercontent.com/JustRedTTG/CornerOS/main/BIOS/bios.lua /tmp/bios.lua && flash -q /tmp/bios.lua CornerOS && reboot")
