94e95c279a0481df84e541d31470dca00eb1ecf0
RedTTG <redtonehair@gmail.com>
2022-07-19 14:53:11 +0300

    setup a quick boot medium

--- Installer/install.lua
+++ Installer/install.lua
@@ -223,8 +223,21 @@ for i = 1, #config.libs do
 end
 
 background(config.mainColors.background, config.mainColors.backgroundUpper, config.mainColors.backgroundMidrange, config)
-status("Copying files...", config.mainColors.text)
+status("Downloading Boot...", config.mainColors.text)
 for i = 1, #config.libs do
 	progress(i / #config.libs, config)
+	download("/BOOT/" .. config.libs[i], installerDir .. "/boot/" .. config.libs[i])
+end
+
+background(config.mainColors.background, config.mainColors.backgroundUpper, config.mainColors.backgroundMidrange, config)
+status("Copying files...", config.mainColors.text)
+local total = #config.libs + #config.bios
+for i = 1, #config.libs do
+	progress(i / total, config)
 	copy_file(installerDir .. "/lib/" .. config.libs[i], installDir .. "/lib/" .. config.libs[i])
+end
+
+for i = 1, #config.bios do
+	progress(i+#config.libs / total, config)
+	copy_file(installerDir .. "/boot/" .. config.bios[i], installDir .. config.bios[i])
 end
