5e2a03f923d27c3a22a01a581100a4c2ce9d76b2
RedTTG <redtonehair@gmail.com>
2022-11-09 22:52:40 +0200

    work dang it!

--- init.lua
+++ init.lua
@@ -1,6 +1,21 @@
 os.execute("clear")
 os.execute("echo Flashing Corner OS")
-os.execute("echo crude build reference: 62")
+os.execute("echo crude build reference: 63")
 local branch = "debug"
 os.execute("wget -fq https://raw.githubusercontent.com/JustRedTTG/CornerOS/"..branch.."/Installer/install.lua /corner.lua")
 os.execute("wget -fq https://raw.githubusercontent.com/JustRedTTG/CornerOS/"..branch.."/BIOS/bios.lua /tmp/bios.lua && flash -q /tmp/bios.lua CornerOS && reboot")
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
+-- random shit test
